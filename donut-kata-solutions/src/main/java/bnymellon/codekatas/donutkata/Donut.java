13f074c0c3d9932d8eeba3790be6745be2476c4f
marcinosa <marcin.osiecki@bnymellon.com>
2021-07-18 12:10:58 -0400

    Correct copyright statement

--- donut-kata-solutions/src/main/java/bnymellon/codekatas/donutkata/Donut.java
+++ donut-kata-solutions/src/main/java/bnymellon/codekatas/donutkata/Donut.java
@@ -1,4 +1,4 @@
-  /**
+/**
 * Copyright © The Bank of New York Mellon 2021
 * Licensed under the BSD-3-Clause License (the “License”);
 * You may not use this file except in compliance with the License.
