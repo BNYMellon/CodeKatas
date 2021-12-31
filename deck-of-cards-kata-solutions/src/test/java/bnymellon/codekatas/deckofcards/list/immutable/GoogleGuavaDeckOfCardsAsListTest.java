8aac11be25c153dd63262dd11b3b669f522a7b1e
Donald Raab <Donald.Raab@bnymellon.com>
2021-12-30 23:03:38 -0500

    Add Guava List solutions for deck-of-cards-kata.
    Signed-off-by: Donald Raab <Donald.Raab@bnymellon.com>

---
+++ deck-of-cards-kata-solutions/src/test/java/bnymellon/codekatas/deckofcards/list/immutable/GoogleGuavaDeckOfCardsAsListTest.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2021 The Bank of New York Mellon.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package bnymellon.codekatas.deckofcards.list.immutable;
+
+import java.util.Random;
+
+import bnymellon.codekatas.deckofcards.Rank;
+import bnymellon.codekatas.deckofcards.Suit;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GoogleGuavaDeckOfCardsAsListTest
+{
+    private JDKImperativeDeckOfCardsAsList jdkDeck = new JDKImperativeDeckOfCardsAsList();
+    private GoogleGuavaDeckOfCardsAsList ggDeck = new GoogleGuavaDeckOfCardsAsList();
+
+    @Test
+    public void allCards()
+    {
+        Assertions.assertEquals(this.jdkDeck.getCards(), this.ggDeck.getCards());
+    }
+
+    @Test
+    public void cardsAreImmutable()
+    {
+        var ggCards = this.ggDeck.getCards();
+        Assertions.assertThrows(
+                UnsupportedOperationException.class,
+                () -> ggCards.remove(0));
+        Assertions.assertThrows(
+                UnsupportedOperationException.class,
+                ggCards::clear);
+        Assertions.assertThrows(
+                UnsupportedOperationException.class,
+                () -> ggCards.add(null));
+    }
+
+    @Test
+    public void diamonds()
+    {
+        Assertions.assertEquals(this.jdkDeck.diamonds(), this.ggDeck.diamonds());
+    }
+
+    @Test
+    public void hearts()
+    {
+        Assertions.assertEquals(this.jdkDeck.hearts(), this.ggDeck.hearts());
+    }
+
+    @Test
+    public void spades()
+    {
+        Assertions.assertEquals(this.jdkDeck.spades(), this.ggDeck.spades());
+    }
+
+    @Test
+    public void clubs()
+    {
+        Assertions.assertEquals(this.jdkDeck.clubs(), this.ggDeck.clubs());
+    }
+
+    @Test
+    public void deal()
+    {
+        var jdkShuffle = this.jdkDeck.shuffle(new Random(1));
+        var ggShuffle = this.ggDeck.shuffle(new Random(1));
+
+        var jdkHand = this.jdkDeck.deal(jdkShuffle, 5);
+        var ggHand = this.ggDeck.deal(ggShuffle, 5);
+        Assertions.assertEquals(jdkHand, ggHand);
+    }
+
+    @Test
+    public void shuffleAndDealHands()
+    {
+        var jdkHands = this.jdkDeck.shuffleAndDeal(new Random(1), 5, 5);
+        var ggHands = this.ggDeck.shuffleAndDeal(new Random(1), 5, 5);
+        Assertions.assertEquals(jdkHands, ggHands);
+    }
+
+    @Test
+    public void dealHands()
+    {
+        var jdkShuffled = this.jdkDeck.shuffle(new Random(1));
+        var ggShuffled = this.ggDeck.shuffle(new Random(1));
+        var jdkHands = this.jdkDeck.dealHands(jdkShuffled, 5, 5);
+        var ggHands = this.ggDeck.dealHands(ggShuffled, 5, 5);
+        Assertions.assertEquals(jdkHands, ggHands);
+    }
+
+    @Test
+    public void cardsBySuit()
+    {
+        var jdkCardsBySuit = this.jdkDeck.getCardsBySuit();
+        var ggCardsBySuit = this.ggDeck.getCardsBySuit();
+        Assertions.assertEquals(jdkCardsBySuit.get(Suit.CLUBS), ggCardsBySuit.get(Suit.CLUBS));
+    }
+
+    @Test
+    public void cardsBySuitIsImmutable()
+    {
+        var ggCardsBySuit = this.ggDeck.getCardsBySuit();
+        Assertions.assertThrows(
+                UnsupportedOperationException.class,
+                () -> ggCardsBySuit.removeAll(Suit.CLUBS));
+        Assertions.assertThrows(
+                UnsupportedOperationException.class,
+                ggCardsBySuit::clear);
+        Assertions.assertThrows(
+                UnsupportedOperationException.class,
+                () -> ggCardsBySuit.get(Suit.CLUBS).remove(0));
+        Assertions.assertThrows(
+                UnsupportedOperationException.class,
+                () -> ggCardsBySuit.get(Suit.CLUBS).add(null));
+        Assertions.assertThrows(
+                UnsupportedOperationException.class,
+                ggCardsBySuit.get(Suit.CLUBS)::clear);
+    }
+
+    @Test
+    public void countsBySuit()
+    {
+        Assertions.assertEquals(
+                this.jdkDeck.countsBySuit().get(Suit.CLUBS).intValue(),
+                this.ggDeck.countsBySuit().count(Suit.CLUBS));
+    }
+
+    @Test
+    public void countsByRank()
+    {
+        Assertions.assertEquals(
+                this.jdkDeck.countsByRank().get(Rank.TEN).intValue(),
+                this.ggDeck.countsByRank().count(Rank.NINE));
+    }
+}
