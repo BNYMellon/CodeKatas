e71e6a992d931724e90733123c3cd8a2e02ca942
Donald Raab <Donald.Raab@bnymellon.com>
2021-12-30 22:41:14 -0500

    Add Vavr SortedSet solutions for deck-of-cards-kata.
    Signed-off-by: Donald Raab <Donald.Raab@bnymellon.com>

---
+++ deck-of-cards-kata-solutions/src/test/java/bnymellon/codekatas/deckofcards/sortedset/immutable/VavrDeckOfCardsAsSortedSetTest.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2021 The Bank of New York Mellon.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package bnymellon.codekatas.deckofcards.sortedset.immutable;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.SortedSet;
+
+import bnymellon.codekatas.deckofcards.Card;
+import bnymellon.codekatas.deckofcards.Rank;
+import bnymellon.codekatas.deckofcards.Suit;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class VavrDeckOfCardsAsSortedSetTest
+{
+    private JDKImperativeDeckOfCardsAsSortedSet jdkDeck = new JDKImperativeDeckOfCardsAsSortedSet();
+    private VavrDeckOfCardsAsSortedSet vavrDeck = new VavrDeckOfCardsAsSortedSet();
+
+    @Test
+    public void allCards()
+    {
+        Assertions.assertEquals(this.jdkDeck.getCards(), this.vavrDeck.getCards().toJavaSet());
+    }
+
+    @Test
+    public void diamonds()
+    {
+        Assertions.assertEquals(this.jdkDeck.diamonds(), this.vavrDeck.diamonds().toJavaSet());
+    }
+
+    @Test
+    public void hearts()
+    {
+        Assertions.assertEquals(this.jdkDeck.hearts(), this.vavrDeck.hearts().toJavaSet());
+    }
+
+    @Test
+    public void spades()
+    {
+        Assertions.assertEquals(this.jdkDeck.spades(), this.vavrDeck.spades().toJavaSet());
+    }
+
+    @Test
+    public void clubs()
+    {
+        Assertions.assertEquals(this.jdkDeck.clubs(), this.vavrDeck.clubs().toJavaSet());
+    }
+
+    @Test
+    public void deal()
+    {
+        Deque<Card> jdkShuffle = this.jdkDeck.shuffle(new Random(1));
+        io.vavr.collection.List<Card> vavrShuffle = this.vavrDeck.shuffle(new Random(1));
+
+        Set<Card> jdkHand = this.jdkDeck.deal(jdkShuffle, 5);
+        Set<Card> vavrHand = this.vavrDeck.deal(vavrShuffle, 5)._1().toJavaSet();
+        Assertions.assertEquals(jdkHand, vavrHand);
+    }
+
+    @Test
+    public void shuffleAndDealHands()
+    {
+        List<Set<Card>> jdkHands = this.jdkDeck.shuffleAndDeal(new Random(1), 5, 5);
+        io.vavr.collection.List<io.vavr.collection.Set<Card>> vavrHands =
+                this.vavrDeck.shuffleAndDeal(new Random(1), 5, 5);
+        Assertions.assertEquals(jdkHands.get(0), vavrHands.get(0).toJavaSet());
+        Assertions.assertEquals(jdkHands.get(1), vavrHands.get(1).toJavaSet());
+        Assertions.assertEquals(jdkHands.get(2), vavrHands.get(2).toJavaSet());
+        Assertions.assertEquals(jdkHands.get(3), vavrHands.get(3).toJavaSet());
+        Assertions.assertEquals(jdkHands.get(4), vavrHands.get(4).toJavaSet());
+    }
+
+    @Test
+    public void dealHands()
+    {
+        Deque<Card> jdkShuffled = this.jdkDeck.shuffle(new Random(1));
+        io.vavr.collection.List<Card> vavrShuffled = this.vavrDeck.shuffle(new Random(1));
+        List<Set<Card>> jdkHands = this.jdkDeck.dealHands(jdkShuffled, 5, 5);
+        io.vavr.collection.List<io.vavr.collection.Set<Card>> vavrHands =
+                this.vavrDeck.dealHands(vavrShuffled, 5, 5);
+        Assertions.assertEquals(jdkHands.get(0), vavrHands.get(0).toJavaSet());
+        Assertions.assertEquals(jdkHands.get(1), vavrHands.get(1).toJavaSet());
+        Assertions.assertEquals(jdkHands.get(2), vavrHands.get(2).toJavaSet());
+        Assertions.assertEquals(jdkHands.get(3), vavrHands.get(3).toJavaSet());
+        Assertions.assertEquals(jdkHands.get(4), vavrHands.get(4).toJavaSet());
+    }
+
+    @Test
+    public void cardsBySuit()
+    {
+        Map<Suit, SortedSet<Card>> jdkCardsBySuit = this.jdkDeck.getCardsBySuit();
+        io.vavr.collection.Map<Suit, ? extends io.vavr.collection.SortedSet<Card>> vavrCardsBySuit =
+                this.vavrDeck.getCardsBySuit();
+        Assertions.assertEquals(new ArrayList<>(jdkCardsBySuit.get(Suit.CLUBS)), vavrCardsBySuit.get(Suit.CLUBS).get().toJavaList());
+    }
+
+    @Test
+    public void countsBySuit()
+    {
+        Assertions.assertEquals(
+                this.jdkDeck.countsBySuit().get(Suit.CLUBS),
+                this.vavrDeck.countsBySuit().get(Suit.CLUBS).get());
+    }
+
+    @Test
+    public void countsByRank()
+    {
+        Assertions.assertEquals(
+                this.jdkDeck.countsByRank().get(Rank.TEN),
+                this.vavrDeck.countsByRank().get(Rank.TEN).get());
+    }
+}
