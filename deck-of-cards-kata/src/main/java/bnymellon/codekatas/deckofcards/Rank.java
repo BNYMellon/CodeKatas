e5e81aad21092b3d1200b10343694f8e6497d676
Emilie <emilie.robichaud@bnymellon.com>
2022-09-30 15:42:39 -0400

    add emoji changes the main repository
    Signed-off-by: Emilie Robichaud <emilie.robichaud@bnymellon.com>

--- deck-of-cards-kata/src/main/java/bnymellon/codekatas/deckofcards/Rank.java
+++ deck-of-cards-kata/src/main/java/bnymellon/codekatas/deckofcards/Rank.java
@@ -18,17 +18,30 @@ package bnymellon.codekatas.deckofcards;
 
 public enum Rank
 {
-    ACE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING
+    ACE("A"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    TEN("10"),
+    JACK("J"),
+    QUEEN("Q"),
+    KING("K");
+
+    private final String emoji;
+
+    Rank(String emoji)
+    {
+        this.emoji = emoji;
+    }
+
+    @Override
+    public String toString()
+    {
+        return emoji;
+    }
 }
