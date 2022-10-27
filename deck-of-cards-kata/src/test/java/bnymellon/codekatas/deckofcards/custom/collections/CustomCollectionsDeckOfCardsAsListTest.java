8e7e94a20f0520d4dd26be3ef452efa7412d93ae
Donald Raab <Donald.Raab@bnymellon.com>
2022-10-26 23:35:01 -0400

    Update Custom Collections Deck Of Cards as List Test
    Signed-off-by: Donald Raab <Donald.Raab@bnymellon.com>

--- deck-of-cards-kata/src/test/java/bnymellon/codekatas/deckofcards/custom/collections/CustomCollectionsDeckOfCardsAsListTest.java
+++ deck-of-cards-kata/src/test/java/bnymellon/codekatas/deckofcards/custom/collections/CustomCollectionsDeckOfCardsAsListTest.java
@@ -16,7 +16,9 @@
 
 package bnymellon.codekatas.deckofcards.custom.collections;
 
+import java.util.Objects;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 import bnymellon.codekatas.deckofcards.Rank;
 import bnymellon.codekatas.deckofcards.Suit;
@@ -38,62 +40,101 @@ public class CustomCollectionsDeckOfCardsAsListTest
     @Test
     public void diamonds()
     {
-        Assertions.assertEquals(this.jdkDeck.diamonds(), this.customDeck.diamonds());
+        Assertions.assertEquals(
+                "|A♦|,|2♦|,|3♦|,|4♦|,|5♦|,|6♦|,|7♦|,|8♦|,|9♦|,|10♦|,|J♦|,|Q♦|,|K♦|",
+                this.customDeck.diamonds().stream().map(Object::toString).collect(Collectors.joining(",")));
     }
 
     @Test
     public void hearts()
     {
-        Assertions.assertEquals(this.jdkDeck.hearts(), this.customDeck.hearts());
+        Assertions.assertEquals(
+                "|A♥|,|2♥|,|3♥|,|4♥|,|5♥|,|6♥|,|7♥|,|8♥|,|9♥|,|10♥|,|J♥|,|Q♥|,|K♥|",
+                this.customDeck.hearts().stream().map(Object::toString).collect(Collectors.joining(",")));
     }
 
     @Test
     public void spades()
     {
-        Assertions.assertEquals(this.jdkDeck.spades(), this.customDeck.spades());
+        Assertions.assertEquals(
+                "|A♠|,|2♠|,|3♠|,|4♠|,|5♠|,|6♠|,|7♠|,|8♠|,|9♠|,|10♠|,|J♠|,|Q♠|,|K♠|",
+                this.customDeck.spades().stream().map(Object::toString).collect(Collectors.joining(",")));
     }
 
     @Test
     public void clubs()
     {
-        Assertions.assertEquals(this.jdkDeck.clubs(), this.customDeck.clubs());
+        Assertions.assertEquals(
+                "|A♣|,|2♣|,|3♣|,|4♣|,|5♣|,|6♣|,|7♣|,|8♣|,|9♣|,|10♣|,|J♣|,|Q♣|,|K♣|",
+                this.customDeck.clubs().stream().map(Object::toString).collect(Collectors.joining(",")));
     }
 
     @Test
     public void deal()
     {
         var jdk1Shuffle = this.jdkDeck.shuffle(new Random(1));
-        var jdk2Shuffle = this.customDeck.shuffle(new Random(1));
+        var customShuffle = this.customDeck.shuffle(new Random(1));
 
         var jdk1Hand = this.jdkDeck.deal(jdk1Shuffle, 5);
-        var jdk2Hand = this.customDeck.deal(jdk2Shuffle, 5);
-        Assertions.assertEquals(jdk1Hand, jdk2Hand);
+        var customHand = this.customDeck.deal(customShuffle, 5);
+        Assertions.assertEquals(jdk1Hand, customHand);
+        Assertions.assertEquals(
+                "|3♦|, |5♥|, |6♥|, |3♣|, |Q♣|",
+                customHand.stream().sorted().map(Object::toString).collect(Collectors.joining(", ")));
     }
 
     @Test
     public void shuffleAndDealHands()
     {
         var jdk1Hands = this.jdkDeck.shuffleAndDeal(new Random(1), 5, 5);
-        var jdk2Hands = this.customDeck.shuffleAndDeal(new Random(1), 5, 5);
-        Assertions.assertEquals(jdk1Hands, jdk2Hands);
+        var customHands = this.customDeck.shuffleAndDeal(new Random(1), 5, 5);
+        Assertions.assertEquals(jdk1Hands, customHands);
+        var hands = customHands.stream().map(each -> each.stream().sorted().map(Objects::toString).collect(Collectors.joining(", "))).toList();
+        ImmutableList<String> expectedHands = ImmutableList.of(
+                "|3♦|, |5♥|, |6♥|, |3♣|, |Q♣|",
+                "|10♠|, |J♠|, |10♥|, |5♣|, |9♣|",
+                "|2♠|, |9♠|, |4♦|, |A♣|, |10♣|",
+                "|Q♠|, |8♦|, |4♥|, |7♣|, |J♣|",
+                "|A♦|, |A♥|, |2♥|, |J♥|, |6♣|");
+        Assertions.assertEquals(expectedHands, hands);
     }
 
     @Test
     public void dealHands()
     {
         var jdk1Shuffled = this.jdkDeck.shuffle(new Random(1));
-        var jdk2Shuffled = this.customDeck.shuffle(new Random(1));
+        var customShuffled = this.customDeck.shuffle(new Random(1));
         var jdk1Hands = this.jdkDeck.dealHands(jdk1Shuffled, 5, 5);
-        var jdk2Hands = this.customDeck.dealHands(jdk2Shuffled, 5, 5);
-        Assertions.assertEquals(jdk1Hands, jdk2Hands);
+        var customHands = this.customDeck.dealHands(customShuffled, 5, 5);
+        Assertions.assertEquals(jdk1Hands, customHands);
+        var hands = customHands.stream().map(each -> each.stream().sorted().map(Objects::toString).collect(Collectors.joining(", "))).toList();
+        ImmutableList<String> expectedHands = ImmutableList.of(
+                "|3♦|, |5♥|, |6♥|, |3♣|, |Q♣|",
+                "|10♠|, |J♠|, |10♥|, |5♣|, |9♣|",
+                "|2♠|, |9♠|, |4♦|, |A♣|, |10♣|",
+                "|Q♠|, |8♦|, |4♥|, |7♣|, |J♣|",
+                "|A♦|, |A♥|, |2♥|, |J♥|, |6♣|");
+        Assertions.assertEquals(expectedHands, hands);
     }
 
     @Test
     public void cardsBySuit()
     {
         var jdk1CardsBySuit = this.jdkDeck.getCardsBySuit();
-        var jdk2CardsBySuit = this.customDeck.getCardsBySuit();
-        Assertions.assertEquals(jdk1CardsBySuit.get(Suit.CLUBS), jdk2CardsBySuit.get(Suit.CLUBS));
+        var customCardsBySuit = this.customDeck.getCardsBySuit();
+        Assertions.assertEquals(jdk1CardsBySuit.get(Suit.CLUBS), customCardsBySuit.get(Suit.CLUBS));
+        Assertions.assertEquals(
+                "|A♣|,|2♣|,|3♣|,|4♣|,|5♣|,|6♣|,|7♣|,|8♣|,|9♣|,|10♣|,|J♣|,|Q♣|,|K♣|",
+                customCardsBySuit.get(Suit.CLUBS).stream().map(Object::toString).collect(Collectors.joining(",")));
+        Assertions.assertEquals(
+                "|A♦|,|2♦|,|3♦|,|4♦|,|5♦|,|6♦|,|7♦|,|8♦|,|9♦|,|10♦|,|J♦|,|Q♦|,|K♦|",
+                customCardsBySuit.get(Suit.DIAMONDS).stream().map(Object::toString).collect(Collectors.joining(",")));
+        Assertions.assertEquals(
+                "|A♠|,|2♠|,|3♠|,|4♠|,|5♠|,|6♠|,|7♠|,|8♠|,|9♠|,|10♠|,|J♠|,|Q♠|,|K♠|",
+                customCardsBySuit.get(Suit.SPADES).stream().map(Object::toString).collect(Collectors.joining(",")));
+        Assertions.assertEquals(
+                "|A♥|,|2♥|,|3♥|,|4♥|,|5♥|,|6♥|,|7♥|,|8♥|,|9♥|,|10♥|,|J♥|,|Q♥|,|K♥|",
+                customCardsBySuit.get(Suit.HEARTS).stream().map(Object::toString).collect(Collectors.joining(",")));
     }
 
     @Test
@@ -120,22 +161,22 @@ public class CustomCollectionsDeckOfCardsAsListTest
     @Test
     public void countsBySuit()
     {
+        MutableBag<Suit> expected = new HashBag<>();
         for (Suit suit : Suit.values())
         {
-            Assertions.assertEquals(
-                    this.jdkDeck.countsBySuit().get(suit).intValue(),
-                    this.customDeck.countsBySuit().getOccurrences(suit));
+            expected.addOccurrences(suit, 13);
         }
+        Assertions.assertEquals(expected, this.customDeck.countsBySuit());
     }
 
     @Test
     public void countsByRank()
     {
+        MutableBag<Rank> expected = new HashBag<>();
         for (Rank rank : Rank.values())
         {
-            Assertions.assertEquals(
-                    this.jdkDeck.countsByRank().get(rank).intValue(),
-                    this.customDeck.countsByRank().getOccurrences(rank));
+            expected.addOccurrences(rank, 4);
         }
+        Assertions.assertEquals(expected, this.customDeck.countsByRank());
     }
 }
